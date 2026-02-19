811680a9b36768b294da00aff06eee1b0cafd2b0
Cuong Dang <70055475+segtreebruh@users.noreply.github.com>
2026-02-19 07:39:43 +0000

    project1

--- os/proc.c
+++ os/proc.c
@@ -2,6 +2,7 @@
 #include "defs.h"
 #include "loader.h"
 #include "trap.h"
+#include "timer.h"
 
 struct proc pool[NPROC];
 char kstack[NPROC][PAGE_SIZE];
@@ -34,6 +35,8 @@ void proc_init(void)
 		/*
 		* LAB1: you may need to initialize your new fields of proc here
 		*/
+		p->time = 0;
+		memset(p->syscall_times, 0, sizeof(p->syscall_times));
 	}
 	idle.kstack = (uint64)boot_stack_top;
 	idle.pid = 0;
@@ -84,6 +87,7 @@ void scheduler(void)
 				/*
 				* LAB1: you may need to init proc start time here
 				*/
+				if (p->time == 0) p->time = get_cycle() * 1000 / CPU_FREQ;
 				p->state = RUNNING;
 				current_proc = p;
 				swtch(&idle.context, &p->context);
